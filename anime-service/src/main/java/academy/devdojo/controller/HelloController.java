8d0f14ab4ce1cbe5e86d070b9f14041511222a27
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-01-07 20:21:52 -0300

    implementing controller class

---
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package academy.devdojo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+   @GetMapping
+    public String hi(){
+        return "Hello World!";
+    }
+
+}
