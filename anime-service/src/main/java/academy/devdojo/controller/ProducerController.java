b363b1c22709766e9b5d2ffadf87c870669b8035
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-01-12 23:18:59 -0300

    creating AnimeRequest and AnimeResponse

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 @RestController
