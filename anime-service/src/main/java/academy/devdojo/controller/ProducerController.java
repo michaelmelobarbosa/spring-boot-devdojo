eab442d5aa2fff716b5aff5713bbcdb100b7e7b6
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-01-12 21:32:59 -0300

    Mapper added

---
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -0,0 +1,54 @@
+package academy.devdojo.controller;
+
+import academy.devdojo.domain.Producer;
+import academy.devdojo.mapper.ProducerMapper;
+import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.response.ProducerGetResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+@RestController
+@RequestMapping("v1/producers")
+@Slf4j
+public class ProducerController {
+    private static final ProducerMapper MAPPER = ProducerMapper.INSTANCE;
+
+
+    @GetMapping()
+    public List<Producer> listAll(@RequestParam(required = false) String name) {
+        List<Producer> producers = Producer.listAllProducers();
+        if (name == null) return producers;
+
+        return Producer.listAllProducers()
+                .stream()
+                .filter(prod -> prod.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    @GetMapping("{id}")
+    public Producer findById(@PathVariable Long id) {
+
+        return Producer.listAllProducers()
+                .stream()
+                .filter(prod -> prod.getId().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProducerGetResponse> save(@RequestBody ProducerPostRequest producerPostRequest) {
+        var producer = MAPPER.toProducer(producerPostRequest);
+        var response = MAPPER.toProducerGetResponse(producer);
+
+        Producer.listAllProducers().add(producer);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+}
---
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -0,0 +1,33 @@
+package academy.devdojo.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+
+public class Producer {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private static List<Producer> producers = new ArrayList<>();
+
+    static {
+        Producer p1 = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
+        Producer p2 = Producer.builder().id(2l).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
+        Producer p3 = Producer.builder().id(3l).name("Mad House").createdAt(LocalDateTime.now()).build();
+
+        producers.addAll(List.of(p1, p2, p3));
+    }
+
+
+    public static List<Producer> listAllProducers() {
+        return producers;
+    }
+}
