07aa7d9324f4d09c00f8bdf5cd846da0ec7ad889
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-02-06 21:13:32 -0300

    commons-core module implemented

--- commons-core/src/main/java/academy/devdojo/exception/NotFoundException.java
+++ commons-core/src/main/java/academy/devdojo/exception/NotFoundException.java
@@ -1,7 +1,6 @@
 package academy.devdojo.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
 public class NotFoundException extends ResponseStatusException {
