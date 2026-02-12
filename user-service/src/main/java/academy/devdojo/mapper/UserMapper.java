5d9a781c45e1eb38c1073da8ae9e98b623b1e27a
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-02-12 16:22:25 -0300

    email validation implemented

--- user-service/src/main/java/academy/devdojo/mapper/UserMapper.java
+++ user-service/src/main/java/academy/devdojo/mapper/UserMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
-    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+
     User toUser(UserPostRequest postRequest);
 
     User toUser(UserPutRequest request);
